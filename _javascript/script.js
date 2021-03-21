16e1cb91289f418fbafabe41efd9eb77fd788cf0
JuanSoulAxel <barruan.mm@gmail.com>
2021-03-21 16:39:09 -0300

    Adicionando bibliotecas ajax para mascarar o telefone

---
+++ _javascript/script.js
@@ -0,0 +1,17 @@
+//biblioteca geral do JQUERY
+src="//code.jquery.com/jquery-1.12.0.min.js";
+
+/*=================FUNÇÃO MOSTRAR BOTÃO COM JQUERY=================*/
+//As funções abaixo farão a interação do botão cadastrar
+
+$("#btnCadastrar").click(function() { //Quando clicar no btnCadastrar
+    $(".tela-cadastrar").show() && $(".corpo").hide() && $(".rodape").hide(); //vai aparecer a tela-cadastrar e sumir com o corpo e rodape
+} );
+
+$(".btnFechar").click(function() { //Quando clicar no btnFechar
+    $(".tela-cadastrar").hide() && $(".corpo").show() && $(".rodape").show();; //vai aparecer com o corpo e rodape e sumir com a tela cadastrar
+} );
+
+/*=================USANDO MÁSCARA COM AJAX PARA TELEFONE=================*/
+
+$("input#txtTelefone").mask("(99) 9999-9999");
--- cadastrar.php
+++ cadastrar.php
@@ -2,11 +2,14 @@
 $nome = $_POST['txtNome'];
 $sobrenome = $_POST['txtSobrenome'];
 $email = $_POST['txtEmail'];
+$telefone = $_POST['txtTelefone'];
 $senha = $_POST['txtSenha'];
+$sexo = $_POST['slcSexo'];
+
 
 $strcon = mysqli_connect('127.0.0.1', 'juan', '123', 'booksahead') or die('Erro ao conectar no banco de dados');
 
-$sql = "INSERT INTO cadastro (nome, sobrenome, email, senha) VALUES ('$nome', '$sobrenome', '$email', '$senha');";
+$sql = "INSERT INTO cadastro (nome, sobrenome, email, telefone, senha, sexo) VALUES ('$nome', '$sobrenome', '$email', '$telefone', '$senha', '$sexo');";
 
 mysqli_query($strcon, $sql) or die("Erro ao tentar cadastrar registro");
 mysqli_close($strcon); //fechar connection
